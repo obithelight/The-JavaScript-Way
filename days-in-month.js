4f409276b63942dc2f98532bbc035ac7edadd4ea
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-20 05:59:10 +0800

    Number of days in a month

---
+++ days-in-month.js
@@ -0,0 +1,4 @@
+/*
+Write a program that accepts a month number (between 1 and 12), then shows the number of days of that month. Leap years are excluded. Incorrect inputs must be taken into account.
+*/
+

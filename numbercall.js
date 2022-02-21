e074d4f1c70d82cceff50a62badc1801c8e24f0d
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-22 07:36:36 +0800

    Compare values between 2 numbers

--- numbercall.js
+++ numbercall.js
@@ -1,3 +1,20 @@
 /*
 Write a program that accepts two numbers, then compares their values and displays an appropriate message in all cases.
 */ 
+
+function numberCall() {
+  let num1 = Number(prompt("Enter number 1"));
+  let num2 = Number(prompt("Enter number 2"));
+
+  if (num1 == "" || num2 == "") {
+    alert("Please Enter a number");
+    numberCall();
+  } else if (num1 == num2) {
+    console.log("The numbers are equal");
+  } else if (num1 > num2) {
+    console.log("The first number is greater than the second");
+  } else {
+    console.log("The second number is greater than the first");
+  }
+}
+numberCall();

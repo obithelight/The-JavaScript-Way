7f9cfa92aaa0524c247dbe1f228ba358f332c505
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-24 08:42:34 +0800

    Final variable values

--- final-values.js
+++ final-values.js
@@ -1,6 +1,6 @@
-//Take a look at the following program.
-//1. nb1 = nb2 = nb3 = 4
+/*Take a look at the following program.*/
 
+//1. nb1 = nb2 = nb3 = 4
 let nb1 = Number(prompt("Enter nb1:")); //4
 let nb2 = Number(prompt("Enter nb2:")); //4
 let nb3 = Number(prompt("Enter nb3:")); //4
@@ -36,3 +36,20 @@ if (nb1 > nb2) {
 }
 console.log(nb1, nb2, nb3); //4, 3, 2
 
+//3. nb1=2, nb2=4, nb3=0
+let nb1 = Number(prompt("Enter nb1:")); //2
+let nb2 = Number(prompt("Enter nb2:")); //4
+let nb3 = Number(prompt("Enter nb3:")); //0
+
+if (nb1 > nb2) {
+  nb1 = nb3 * 2;
+} else {
+  nb1++; //nb1 = 3
+  if (nb2 > nb3) {
+    nb1 += nb3 * 3; //nb1 = 3
+  } else {
+    nb1 = 0;
+    nb3 = nb3 * 2 + nb2;
+  }
+}
+console.log(nb1, nb2, nb3); //3, 4, 0

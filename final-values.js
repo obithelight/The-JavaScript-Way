2b7a4d35d4cd22a8b81c3de03ea1c0fdc0e77a35
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-24 08:39:19 +0800

    final variable values

--- final-values.js
+++ final-values.js
@@ -18,3 +18,21 @@ if (nb1 > nb2) {
 }
 console.log(nb1, nb2, nb3); //0, 4, 12
 
+//2. nb1=4, nb2=3, nb3=2
+let nb1 = Number(prompt("Enter nb1:")); //4
+let nb2 = Number(prompt("Enter nb2:")); //3
+let nb3 = Number(prompt("Enter nb3:")); //2
+
+if (nb1 > nb2) {
+  nb1 = nb3 * 2; //nb1 = 4
+} else {
+  nb1++;
+  if (nb2 > nb3) {
+    nb1 += nb3 * 3;
+  } else {
+    nb1 = 0;
+    nb3 = nb3 * 2 + nb2;
+  }
+}
+console.log(nb1, nb2, nb3); //4, 3, 2
+

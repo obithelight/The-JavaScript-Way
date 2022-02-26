e8c033cd8121b6fb9cb75fab0e21c1d2df763eee
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-26 11:19:15 +0800

    Number of days in a month

--- days-in-month.js
+++ days-in-month.js
@@ -2,41 +2,61 @@
 Write a program that accepts a month number (between 1 and 12), then shows the number of days of that month. Leap years are excluded. Incorrect inputs must be taken into account.
 */
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+//Solution 1: Function Method
 function monthNos() {
-  const months = [
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-    "july",
-    "august",
-    "september",
-    "october",
-    "november",
-    "december"
-  ];
+  let input = Number(prompt("Please enter a number between 1 and 12"));
 
-  let input = Number(prompt("Enter a Number for a month between 1 and 12"));
-
-  if (input == 4 || input == 6 || input == 9 || input == 11) {
-    console.log(months[input - 1] + " has 30 days");
-  } else if (
-    input == 1 ||
-    input == 3 ||
-    input == 5 ||
-    input == 7 ||
-    input == 8 ||
-    input == 10 ||
-    input == 12
-  ) {
-    console.log(months[input - 1] + " has 31 days");
-  } else if (input == 2) {
-    console.log(months[input - 1] + " has 28 days");
-  } else {
-    alert("Wrong entry. Try again.");
+  if (input < 1 || input > 12) {
+    alert("Wrong entry. Please try again.");
     monthNos();
+  } else if (input == 2) {
+    console.log(
+      months[input - 1] +
+        " has 28 days in a non-leap year & 29 days in a leap year."
+    );
+  } else if (input == 4 || input == 6 || input == 9 || input == 11) {
+    console.log(months[input - 1] + " has 30 days in a year.");
+  } else {
+    console.log(months[input - 1] + " has 31 days in a year.");
   }
 }
 monthNos();
+
+//Solution 2: Switch Method
+let monthNos = 0;
+do {
+  monthNos = Number(prompt("Please enter a number between 1 and 12"));
+} while (monthNos < 1 || monthNos > 12);
+
+switch (monthNos) {
+  case 2:
+    console.log(
+      months[monthNos - 1] +
+        " has 28 days in a non-leap year & 29 days in a leap year."
+    );
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    console.log(months[monthNos - 1] + " has 30 days in a year.");
+    break;
+  default:
+    console.log(months[monthNos - 1] + " has 31 days in a year.");
+    break;
+}

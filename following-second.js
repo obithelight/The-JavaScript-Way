0a340fb1dae6f374c39fa4cea69a61f91bde210d
Kenneth Onu <kennetho.onu@gmail.com>
2022-02-28 19:48:15 +0800

    Hours, seconds, & minutes

---
+++ following-second.js
@@ -0,0 +1,9 @@
+/*
+Write a program that asks for a time under the form of three information (hours, minutes, seconds). The program calculates and shows the time one second after. Incorrect inputs must be taken into account.
+
+This is not as simple as it seems... Look at the following results to see for yourself:
+
+14h17m59s => 14h18m0s
+6h59m59s => 7h0m0s
+23h59m59s => 0h0m0s (midnight)
+*/

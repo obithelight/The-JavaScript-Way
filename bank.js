9de1a5e49b7095c2014659025ff420fa3679fe1a
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-03 19:18:29 +0800

    Modeling a bank account

--- bank.js
+++ bank.js
@@ -8,3 +8,12 @@ Write a program that creates an account object with the following characteristic
 //A credit method adding the (positive or negative) value passed as an argument to the account balance.
 //A describe method returning the account description.
 //Use this object to show its description, crediting 250, debiting 80, then show its description again.
+
+let bankAccount = {
+  name: "Alex",
+  balance: 0,
+  
+  amount(){
+    this.creditAmount = +prompt("Enter credit amount")
+    this.debitAmount = +prompt("Enter debit amount")
+  },

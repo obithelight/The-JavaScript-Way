74f63ade49bb4c882be2c50c628c79ccd2059bdd
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-03 19:17:43 +0800

    Modeling a bank account

--- bank.js
+++ bank.js
@@ -2,3 +2,9 @@
 Modeling a bank account
 Write a program that creates an account object with the following characteristics:
 */
+
+//A name property set to "Alex".
+//A balance property set to 0.
+//A credit method adding the (positive or negative) value passed as an argument to the account balance.
+//A describe method returning the account description.
+//Use this object to show its description, crediting 250, debiting 80, then show its description again.

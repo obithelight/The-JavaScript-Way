bc27f2f3cc26ed26bdb2f27af0e7c5321b4ca2a9
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-05 06:43:14 +0800

    Modeling a circle

--- circle.js
+++ circle.js
@@ -2,3 +2,5 @@
 Modeling a circle
 Complete the following program to add the circle object definition. Its radius value is input by the user.
 */
+
+const r = Number(prompt("Enter the circle radius:"));

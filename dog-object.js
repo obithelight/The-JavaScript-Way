d64fa9257d627c07fd8a58f6f5d38d9985a2a3e7
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-06 06:16:44 +0800

    Create a dog object

--- dog-object.js
+++ dog-object.js
@@ -17,3 +17,4 @@ let dog = {
 console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`); //"Sheryl is a Chuwawa dog measuring 6 ft"
 console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`); //"Look, a cat! Sheryl barks: Ho-Ho-Ho"
 console.log(dog.bark());// 1. "Ho-Ho-Ho"
+//2. "Sheryl is a 6 ft Chuwawa breed"

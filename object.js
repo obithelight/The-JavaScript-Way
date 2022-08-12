3a507ed7c544568a09c8c4d6f25021494b7ca51d
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-12 21:50:57 +0800

    Introduction to objects

--- object.js
+++ object.js
@@ -2,3 +2,6 @@
 Adding character experience
 Improve our example RPG program to add an experience property named xp to the character. Its initial value is 0. Experience must appear in character description.
 */
+
+// TODO: create the character object here
+

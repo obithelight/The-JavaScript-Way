5d7158487b87d7ceeee4f142808e068ac5c5237e
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-15 19:36:44 +0800

    Circumference of a circle

---
+++ circumference.js
@@ -0,0 +1,9 @@
+/*
+Circumference and area of a circle
+Write a program containing two functions to calculate the circumference and area of a circle defined by its radius. Test it using user input.
+
+Here are some tips for solving this exercise:
+
+Circumference and area calculation formulas should be part of your secondary school memories... Or a Google click away :)
+The value of number π (Pi) is obtained with Math.PI in JavaScript.
+*/

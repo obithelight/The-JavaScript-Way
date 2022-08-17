96deec4b86c13b86e48d59424a0d890ff45df548
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-17 13:36:31 +0800

    Basic Arithmetical Operations

--- calculator.js
+++ calculator.js
@@ -3,4 +3,5 @@ Calculator
 Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
 */
 
-// TODO: Complete program
+// TODO: (SWITCH STATEMENT)
+

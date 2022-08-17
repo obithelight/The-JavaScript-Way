e93e5aa7b292af78d0e2acaa3c6f87bcb8fec3fe
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-17 13:38:17 +0800

    Basic Arithmetical Operations

--- calculator.js
+++ calculator.js
@@ -20,3 +20,8 @@ function calculate(num1, operator, num2){
       break;
   }
 }
+
+console.log(calculate(4, "+", 6));  // Must show 10
+console.log(calculate(4, "-", 6));  // Must show -2
+console.log(calculate(2, "*", 0));  // Must show 0
+console.log(calculate(12, "/", 0)); // Must show Infinity

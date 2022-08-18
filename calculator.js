24c0f08b7cb4b410382bd71f30507051a5b52f4e
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-18 13:31:14 +0800

    Minimum of two Numbers

---
+++ calculator.js
@@ -0,0 +1,4 @@
+/*
+Calculator
+Complete the following program so that it offers the four basic arithmetical operations: addition, subtraction, multiplication and division. You can use either a function declaration or a function expression.
+*/

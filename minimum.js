bde75d07d1a3ff45a839f16fd8cffe0edaec780f
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-20 12:20:18 +0800

    Minimum of two Numbers

---
+++ minimum.js
@@ -0,0 +1,6 @@
+/*
+Minimum of two numbers
+Let's pretend the JavaScript Math.min() function doesn't exist. Complete the following program so that the min() function returns the minimum of its two received numbers.
+*/
+
+

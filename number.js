54accf49ddd188b54e97fe06bebdf2bd0d05008f
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-22 11:34:38 +0800

    Number Squaring

--- number.js
+++ number.js
@@ -17,3 +17,6 @@ const square2 = x => x ** 2
 console.log(square2(0)); // Must show 0
 console.log(square2(2)); // Must show 4
 console.log(square2(5)); // Must show 25
+
+//When it's done, update the program so that it shows the square of every number between 0 and 10.
+

84914c9a3c8a7cc4cb7ff17136e38e88df25ed83
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-23 11:30:01 +0800

    Number Squaring

--- number.js
+++ number.js
@@ -3,3 +3,8 @@ Number squaring
 Complete the following program so that the square1() and square2() functions work properly.
 */
 
+// Square the given number x
+function square1(x) {
+  return Math.pow(x, 2)
+}
+

bf7d2d3e879bba766c15da4ce6da35b6de08f844
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-23 11:32:40 +0800

    Number Squaring

--- number.js
+++ number.js
@@ -7,7 +7,9 @@ Complete the following program so that the square1() and square2() functions wor
 function square1(x) {
   return Math.pow(x, 2)
 }
-
 console.log(square1(0)); // Must show 0
 console.log(square1(2)); // Must show 4
 console.log(square1(5)); // Must show 25
+
+// Square the given number x
+const square2 = x => x ** 2

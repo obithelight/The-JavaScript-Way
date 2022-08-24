b1973d2fad6c6cfe2e12865bd9d5ee6e801501ab
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-24 09:22:55 +0800

    Program FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -23,3 +23,13 @@ fizzBuzz()
 
 //Improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
 
+function fizzBuzz(){
+  for(let i = 1; i <= 100; i++){
+    if(i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz")
+    else if(i % 3 == 0) console.log("Fizz")
+    else if(i % 5 == 0) console.log("Buzz")
+    else console.log(i)  
+  }
+  
+}
+fizzBuzz()

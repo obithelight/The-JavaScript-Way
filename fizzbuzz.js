5173c230b2a1d9593dede1ac606ce8f99e8985c1
Kenneth Onu <kennetho.onu@gmail.com>
2022-08-25 09:20:46 +0800

    Program FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,3 +10,4 @@ It shows "Buzz" instead if the number is divisible by 5 and not by 3.
 When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
 */
 
+//Solution

c52c395eea287f14146ea0875531515ed3973cd6
Kenneth Onu <kennetho.onu@gmail.com>
2022-11-24 07:57:12 +0800

    Function to return Yes or No

---
+++ yes-no.js
@@ -0,0 +1,3 @@
+/*Neither Yes nor No
+Write a program that plays "neither yes, nor no" with the user. Specifically, the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
+*/
